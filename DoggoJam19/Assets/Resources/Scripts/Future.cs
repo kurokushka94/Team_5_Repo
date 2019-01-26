8e53afd2ec68b6f9d440fea24bfb0e4dbc34dd9f
Duncan <dawhiteside@fullsail.edu>
2019-01-25 22:58:03 -0500

    Basic Future Past Utility

---
+++ DoggoJam19/Assets/Resources/Scripts/Future.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Future : MonoBehaviour
+{
+
+    protected Utility.ActivateStartDelegate ActivateStart;
+
+    public void AddActivateStartFunction(Utility.ActivateStartDelegate _startFunction)
+    {
+        ActivateStart += _startFunction;
+    }
+
+    public virtual void FinishUpdate(GameObject _targetObject)
+    {
+        if (_targetObject != null)
+            gameObject.transform.position = _targetObject.transform.position + Utility.GetOffset();
+    }
+    public virtual void ActivateObject()
+    {
+        if (ActivateStart != null)
+            ActivateStart();
+    }
+
+}
