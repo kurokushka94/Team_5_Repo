3a927718a49d612a4bda727c085c9a48b564a8c6
Dylan Scheuring <dylanscheuring@gmail.com>
2019-01-26 13:04:38 -0500

    Modular trigger system put in place. GenericListener.cs is the "Door", that waits for all required events to occur. GenericSwitch.cs and GenericTrigger.cs both trigger events to occur for the Listener.

---
+++ DoggoJam19/Assets/Resources/Scripts/GenericSwitch.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSwitch : BaseCondition
+{
+	protected virtual void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.name == TargetName || other.gameObject.tag == TargetTag)
+		{
+			IsActivated = !IsActivated;
+		}
+	}
+}
