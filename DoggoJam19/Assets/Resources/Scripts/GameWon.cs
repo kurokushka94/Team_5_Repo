ce5798331a81399a8d39c4851884d6457919affd
Ivan Zadniprovskyi <izadniprovskyi@student.fullsail.edu>
2019-01-27 08:19:50 -0500

    Fixed collectable

---
+++ DoggoJam19/Assets/Resources/Scripts/GameWon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWon : MonoBehaviour
+{
+    private int score;
+
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score == 3)
+        {
+            transform.GetComponent<Canvas>().enabled = true;
+            Time.timeScale = 0;
+        }
+    }
+
+    void AddScore()
+    {
+        score += 1;
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
